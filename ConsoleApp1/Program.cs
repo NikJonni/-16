1a510f95a877da97791425fc8425a983ffd44450
Никита Егоров <nik.egorov1994@mail.ru>
2021-12-24 15:20:13 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\Папка\\Products.json";
+            string jsonString = File.ReadAllText(path);
+            Product[] products = JsonSerializer.Deserialize<Product[]>(jsonString);
+            double max = products[0].ProductPrice;
+            string maxexpensive = products[0].ProductName;
+            for (int i = 1; i < 5; i++)
+            {
+                if (products[i].ProductPrice > max)
+                {
+                    max = products[i].ProductPrice;
+                    maxexpensive = products[i].ProductName;
+                }
+            }
+            Console.WriteLine("Самый дорогой товар: {0}", maxexpensive);
+            Console.ReadKey();
+        }
+        class Product
+        {
+            public int ProductCode { get; set; }
+            public string ProductName { get; set; }
+            public double ProductPrice { get; set; }
+        }
+    }
+}
---
+++ Задание 16/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+
+namespace Задание_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\Папка\\Products.json";
+            if (!File.Exists(path))
+                File.Create(path);
+            Product[] products = new Product[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Введите код, название и цену товара:");
+                products[i] = new Product()
+                {
+                    ProductCode = Convert.ToInt32(Console.ReadLine()),
+                    ProductName = Convert.ToString(Console.ReadLine()),
+                    ProductPrice = Convert.ToDouble(Console.ReadLine())
+                };
+            }
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+            string jsonString = JsonSerializer.Serialize(products, options);
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+                sw.WriteLine(jsonString);
+            }
+            Console.ReadKey();
+        }
+    }
+    class Product
+    {
+        public int ProductCode { get; set; }
+        public string ProductName { get; set; }
+        public double ProductPrice { get; set; }
+    }
+}
